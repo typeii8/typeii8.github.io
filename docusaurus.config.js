11386126579409cced1f9fbe9712667fb7e3e102
Zoe <zoe.huang@neutec.com.tw>
2023-03-08 15:34:34 +0800

    doc structure add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,12 +77,12 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'intro',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             to: '/about',
